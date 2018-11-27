3dccb01508591c23d6f21f2cc8fa550f8254075a
leo <leopangco@yahoo.com>
2018-11-27 11:56:26 -0800

    added some extra methods needing to be worked on in pass1. Read comments associated with each function

--- Lab 6/Pass1Visitor.cpp
+++ Lab 6/Pass1Visitor.cpp
@@ -31,7 +31,7 @@ antlrcpp::Any Pass1Visitor::visitProgram(MainParser::ProgramContext *ctx)
 {
     auto value = visitChildren(ctx);
 
-//    cout << "=== visitProgram: Printing xref table." << endl;
+    cout << "=== visitProgram: Printing xref table." << endl;
 
     // Print the cross-reference table.
     CrossReferencer cross_referencer;
@@ -93,6 +93,152 @@ antlrcpp::Any Pass1Visitor::visitBlock(MainParser::BlockContext *ctx)
 
      return value;
  }
+
+
+//Leo: Wondering how we'll figure out how to access such 'ctx' functions correctly
+//Replaced Float with Char type condition
+antlrcpp::Any Pass1Visitor::visitTypeId(MainParser::TypeIdContext *ctx)
+{
+    cout << "=== visitTypeId: " + ctx->getText() << endl;
+
+    TypeSpec *type;
+    string type_indicator;
+
+    string type_name = ctx->IDENTIFIER()->toString();
+    if (type_name == "integer")
+    {
+        type = Predefined::integer_type;
+        type_indicator = "I";
+    }
+    else if (type_name == "char")
+    {
+        type = Predefined::char_type;
+        type_indicator = "C";
+    }
+    else
+    {
+        type = nullptr;
+        type_indicator = "?";
+    }
+
+    for (SymTabEntry *id : variable_id_list) {
+        id->set_typespec(type);
+
+        // Emit a field declaration.
+        j_file << ".field private static "
+               << id->get_name() << " " << type_indicator << endl;
+    }
+
+    return visitChildren(ctx);
+}
+
+
+//Leo: How are we to implement the real_mode stuff using char?
+antlrcpp::Any Pass1Visitor::visitAddSubExpr(MainParser::AddSubExprContext *ctx)
+{
+    cout << "=== visitAddSubExpr: " + ctx->getText() << endl;
+
+    auto value = visitChildren(ctx);
+
+    TypeSpec *type1 = ctx->expr(0)->type;
+    TypeSpec *type2 = ctx->expr(1)->type;
+
+    bool integer_mode =    (type1 == Predefined::integer_type)
+                        && (type2 == Predefined::integer_type);
+    //Need to figure this stuff out
+    bool real_mode    =    (type1 == Predefined::real_type)
+                        && (type2 == Predefined::real_type);
+
+    TypeSpec *type = integer_mode ? Predefined::integer_type
+                   : real_mode    ? Predefined::real_type
+                   :                nullptr;
+    ctx->type = type;
+
+    return value;
+}
+
+
+//Another situation where we need to account for Float type
+antlrcpp::Any Pass1Visitor::visitMulDivExpr(MainParser::MulDivExprContext *ctx)
+{
+//    cout << "=== visitMulDivExpr: " + ctx->getText() << endl;
+
+    auto value = visitChildren(ctx);
+
+    TypeSpec *type1 = ctx->expr(0)->type;
+    TypeSpec *type2 = ctx->expr(1)->type;
+
+    bool integer_mode =    (type1 == Predefined::integer_type)
+                        && (type2 == Predefined::integer_type);
+    //CHAR STUFF
+    bool real_mode    =    (type1 == Predefined::real_type)
+                        && (type2 == Predefined::real_type);
+
+    TypeSpec *type = integer_mode ? Predefined::integer_type
+                   : real_mode    ? Predefined::real_type
+                   :                nullptr;
+    ctx->type = type;
+
+    return value;
+}
+
+
+
+/*
+ *
+ antlrcpp::Any Pass1Visitor::visitVariableExpr(Pcl2Parser::VariableExprContext *ctx)
+{
+//    cout << "=== visitVariableExpr: " + ctx->getText() << endl;
+
+    string variable_name = ctx->variable()->IDENTIFIER()->toString();
+    SymTabEntry *variable_id = symtab_stack->lookup(variable_name);
+
+    ctx->type = variable_id->get_typespec();
+    return visitChildren(ctx);
+}
+
+
+ antlrcpp::Any Pass1Visitor::visitSignedNumber(Pcl2Parser::SignedNumberContext *ctx)
+{
+//    cout << "=== visitSignedNumber: " + ctx->getText() << endl;
+
+    auto value = visit(ctx->number());
+    ctx->type = ctx->number()->type;
+    return value;
+}
+
+antlrcpp::Any Pass1Visitor::visitUnsignedNumberExpr(Pcl2Parser::UnsignedNumberExprContext *ctx)
+{
+//    cout << "=== visitUnsignedNumberExpr: " + ctx->getText() << endl;
+
+    auto value = visit(ctx->number());
+    ctx->type = ctx->number()->type;
+    return value;
+}
+
+antlrcpp::Any Pass1Visitor::visitIntegerConst(Pcl2Parser::IntegerConstContext *ctx)
+{
+//    cout << "=== visitIntegerConst: " + ctx->getText() << endl;
+
+    ctx->type = Predefined::integer_type;
+    return visitChildren(ctx);
+}
+
+antlrcpp::Any Pass1Visitor::visitFloatConst(Pcl2Parser::FloatConstContext *ctx)
+{
+//    cout << "=== visitFloatConst: " + ctx->getText() << endl;
+
+    ctx->type = Predefined::real_type;
+    return visitChildren(ctx);
+}
+
+
+
+
+ * */
+
+
+
 
 antlrcpp::Any Pass1Visitor::visitDeclaration_stmt(MainParser::Declaration_stmtContext *ctx)
 {
